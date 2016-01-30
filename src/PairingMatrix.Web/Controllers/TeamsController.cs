a470b0078f761730d6e2a807c6959059a4ed1dbc
Bilal Fazlani <bfazlani@seek.com.au>
2016-01-31 00:18:18 +0530

    Added empty teams page and basic navigation in layout, some colors

---
+++ src/PairingMatrix.Web/Controllers/TeamsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using PairingMatrix.ServiceClient.Interfaces;
+using PairingMatrix.ServiceClient.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PairingMatrix.Web.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly ICRUD<Team> _teamService;
+
+        public TeamsController(ICRUD<Team> teamService)
+        {
+            _teamService = teamService;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var data = await _teamService.GetPagedAsync(0, 100);
+            return View(data);
+        }
+    }
+}
