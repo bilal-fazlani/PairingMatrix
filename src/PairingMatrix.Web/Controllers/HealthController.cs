74fc17343356f9718c18830c8ad5193858e5ae98
Bilal Fazlani <bfazlani@seek.com.au>
2016-01-31 13:40:17 +0530

    added a health controller

---
+++ src/PairingMatrix.Web/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace PairingMatrix.Web.Controllers
+{
+    public class HealthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("OK");
+        }
+    }
+}
